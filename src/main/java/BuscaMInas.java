a343cb133e0a1c51b1cc1a3a953b472386eadd66
pcarroza <pcarroza91@gmail.com>
2021-01-17 13:35:57 -0300

    add

--- src/main/java/BuscaMInas.java
+++
@@ -1,7 +0,0 @@
-package main.java;
-
-public class BuscaMInas {
-    public static void main(String[] args) {
-        System.out.println("lenguaje");
-    }
-}
---
+++ src/main/java/BuscaMinas.java
@@ -0,0 +1,22 @@
+package main.java;
+
+public abstract class BuscaMInas {
+
+    private Logic logic;
+
+    private View view;
+
+    public void run() {
+        Controller controller;
+        do {
+            
+        } while (controller != null);
+    }
+
+    public static void main(String[] args) {
+
+        int number = 200;
+
+
+    }
+}
