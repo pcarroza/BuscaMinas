153baa4553cfb0ad332f3ee90f4329c73ace12f2
pcarroza <pcarroza91@gmail.com>
2021-03-16 20:05:32 -0300

    tests

---
+++ src/tests/main/java/models/GameTest.java
@@ -0,0 +1,18 @@
+package main.java.models;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void isInclude() {
+        assertEquals(true, true);
+    }
+}
